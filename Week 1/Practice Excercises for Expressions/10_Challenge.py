fd711593881942a4f1cf28e2edc29d1e5a16dc26
smokeyrd <smokeyrd@users.noreply.github.com>
2018-01-24 16:23:17 -0500

    first week- practice excercises for expressions
    Signed-off-by: smokeyrd <smokeyrd@users.noreply.github.com>

--- Week 1/Practice Excercises for Expressions/10_Challenge.py
+++ Week 1/Practice Excercises for Expressions/10_Challenge.py
@@ -1,4 +1,4 @@
-#The distance between two points (x0,y0) and (x1,y1) is (x0−x1)2+(y0−y1)2−−−−−−−−−−−−−−−−−−√.
+##The distance between two points (x0,y0) and (x1,y1) is (x0−x1)2+(y0−y1)2−−−−−−−−−−−−−−−−−−√.
 # Write a Python statement that calculates and prints the distance between the points (2,2) and (5,6).
 # (Hint: Remember that a square root can be computed by raising a value to the 0.5 power.)
 
